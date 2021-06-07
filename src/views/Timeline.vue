69b81b437c6123745180d64d62a11bd8c8d6f53d
const39 <const8539@gmail.com>
2021-06-07 15:34:42 +0200

    Add animation on item hover

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -1,10 +1,15 @@
 <template>
 	<v-container>
 		<v-timeline>
-			<v-timeline-item v-for="event in liveData.events" :key="event.id" :icon="icons.whichEventIcon(event.type)" fill-dot>
+			<v-timeline-item
+				v-for="event in liveData.events"
+				:key="event.id"
+				:icon="icons.whichEventIcon(event.type)"
+				fill-dot
+			>
 				<v-card class="elevation-2">
-					<v-card-title class="text-subtitle-2"> {{"title"}} </v-card-title>
-					<v-card-text> {{event.desc}} </v-card-text>
+					<v-card-title class="text-subtitle-2"> {{ "title" }} </v-card-title>
+					<v-card-text> {{ event.desc }} </v-card-text>
 				</v-card>
 			</v-timeline-item>
 		</v-timeline>
@@ -18,12 +23,28 @@ export default {
 	data() {
 		return {
 			liveData: storage.data,
-			icons: icons
-		}
+			icons: icons,
+		};
 	},
-}
+};
 </script>
 
 <style>
+/* Set zoom on hover animation on the compiled Vuetify class of the dot */
+.v-timeline-item__dot:hover {
+	animation-name: zoom-on-hover;
+	animation-iteration-count: 1;
+	animation-timing-function: ease-in;
+	animation-duration: 0.1s;
+	transform: scale(1.25);
+}
 
+@keyframes zoom-on-hover {
+	0% {
+		transform: scale(1);
+	}
+	100% {
+		transform: scale(1.25);
+	}
+}
 </style>
