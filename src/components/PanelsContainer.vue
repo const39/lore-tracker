5f497659bd65c9c56dac4e519b5bf3a91569b8f3
const39 <const8539@gmail.com>
2021-06-10 15:48:34 +0200

    Remove ConsecutivePanel display mode + delete HelloWorld boilerplate

--- src/components/PanelsContainer.vue
+++ src/components/PanelsContainer.vue
@@ -4,9 +4,6 @@
 			<div class="text-xl-h4">Lore tracker</div>
 			<v-spacer></v-spacer>
 			<div class="display-selector">
-				<v-btn icon :color="color('consecutive')" @click="onClick('consecutive')">
-					<v-icon>mdi-view-list-outline</v-icon>
-				</v-btn>
 				<v-btn icon :color="color('tabbed')" @click="onClick('tabbed')">
 					<v-icon>mdi-tab</v-icon>
 				</v-btn>
@@ -17,20 +14,17 @@
 		</div>
 
 		<TabbedPanels v-if="selectedDisplay == 'tabbed'"></TabbedPanels>
-		<ConsecutivePanels v-else-if="selectedDisplay == 'consecutive'"></ConsecutivePanels>
 		<ColumnPanels v-else-if="selectedDisplay == 'column'"></ColumnPanels>
 	</div>
 </template>
 
 <script>
-import ConsecutivePanels from "./ConsecutivePanels.vue";
 import TabbedPanels from "./TabbedPanels.vue";
 import ColumnPanels from "./ColumnPanels.vue";
 
 export default {
 	name: "PanelsContainer",
 	components: {
-		ConsecutivePanels,
 		TabbedPanels,
 		ColumnPanels,
 	},
