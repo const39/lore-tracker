3c81007d5dc00d347f18fbaaef166cb3699c9335
const39 <const8539@gmail.com>
2021-06-10 14:32:21 +0200

    Add edit feature on Notes

--- src/components/cards/NoteCard.vue
+++ src/components/cards/NoteCard.vue
@@ -1,5 +1,9 @@
 <template>
 	<v-card class="mb-4">
+		<v-card-actions class="float-right">
+			<CardOptions @option-selected="onOptionSelected"></CardOptions>
+			<NoteForm v-model="showEditDialog" edit :id="id"></NoteForm>
+		</v-card-actions>
 		<v-card-text class="pa-3">
 			<p class="text-h6 text--primary">{{ title }}</p>
 			<p class="text--primary">{{ desc }}</p>
@@ -8,12 +12,30 @@
 </template>
 
 <script>
+import NoteForm from '../forms/NoteForm.vue'
+import CardOptions from './CardOptions.vue'
+
 export default {
     name: "LocationCard",
+    components: {
+        CardOptions,
+        NoteForm
+    },
     props: {
         id: Number,
         title: String,
         desc: String
+    },
+    data() {
+        return {
+            showEditDialog: false
+        }
+    },
+    methods: {
+		onOptionSelected(value) {
+			if (value === "edit") this.showEditDialog = true;
+			// else if (value === "delete") console.log('delete');
+		},
     }
 };
 </script>
--- src/components/forms/NoteForm.vue
+++ src/components/forms/NoteForm.vue
@@ -3,7 +3,8 @@
 		<v-form v-model="valid" ref="form">
 			<v-card>
 				<v-card-title>
-					<span class="text-h5">Ajouter une note</span>
+					<span class="text-h5" v-if="edit">Modifier une note</span>
+					<span class="text-h5" v-else>Ajouter une note</span>
 				</v-card-title>
 				<v-card-text>
 					<v-container>
@@ -33,6 +34,8 @@ import storage from "../../js/storage.js";
 export default {
 	props: {
 		value: Boolean, // Default v-model overwrite
+		id: Number,
+		edit: Boolean
 	},
 	data() {
 		return {
@@ -44,14 +47,37 @@ export default {
 	methods: {
 		submit() {
 			this.$refs.form.validate();
+
 			if (this.valid) {
-				this.noteModel.id = storage.uid();
-				storage.data.notes.push(this.noteModel);
+
+				// In edit mode
+				if(this.edit) {
+					
+					let index = storage.data.notes.findIndex(entry => entry.id === this.id);
+
+					// We use this.$set() to replace the object at index with our new model while allowing Vue to still track changes to that object
+					// @see https://vuejs.org/v2/guide/reactivity.html#For-Arrays
+					if(index != -1) this.$set(storage.data.notes, index, this.noteModel);
+					else console.error("Could not save the edit.");
+
+				// In create mode
+				} else {
+					this.noteModel.id = storage.uid();
+					storage.data.notes.push(this.noteModel);
+				}
+
 				storage.persist();
 				this.showDialog = false;
 			}
 		},
 		initModel() {
+			if (this.edit && this.id) {
+				let data = storage.data.notes.find((entry) => entry.id === this.id);
+
+				// We return a clone of the object to avoid modifying directly the store
+				// Helpful when the user cancels their changes because we don't have to rollback
+				if (data) return storage.clone(data);
+			}
 			return {
 				title: "",
 				desc: "",
