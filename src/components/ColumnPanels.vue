9cc14af30a1b483e02c728ed763a75773bf94eb7
const39 <const8539@gmail.com>
2021-06-14 19:41:36 +0200

    Add ability to collapse each column

--- src/components/ColumnPanels.vue
+++ src/components/ColumnPanels.vue
@@ -2,130 +2,150 @@
 	<v-container>
 		<v-row>
 			<v-col cols="12" md="4" sm="2">
-				<v-card>
-					<v-card-title> {{ panels[0] }} </v-card-title>
-					<v-card-text>
-						<draggable
-							v-model="liveData.objectives"
-							group="objectives"
-							@start="drag = true"
-							@end="
-								drag = false;
-								persist();
-							"
-						>
-							<ObjectiveCard
-								class="draggable"
-								v-for="objective in liveData.objectives"
-								:key="objective.id"
-								v-bind="objective"
-							></ObjectiveCard>
-						</draggable>
-						<AddCard @add-card-clicked="showObjectiveForm = true"></AddCard>
-						<ObjectiveForm v-model="showObjectiveForm"></ObjectiveForm>
-					</v-card-text>
-				</v-card>
+				<v-expansion-panels v-model="isObjectiveCollapsed">
+					<v-expansion-panel>
+						<v-expansion-panel-header class="text-h5 text--primary">
+							{{ panels[0] }}
+						</v-expansion-panel-header>
+						<v-expansion-panel-content>
+							<draggable
+								v-model="liveData.objectives"
+								group="objectives"
+								@start="drag = true"
+								@end="
+									drag = false;
+									persist();
+								"
+							>
+								<ObjectiveCard
+									class="draggable"
+									v-for="objective in liveData.objectives"
+									:key="objective.id"
+									v-bind="objective"
+								></ObjectiveCard>
+							</draggable>
+							<AddCard @add-card-clicked="showObjectiveForm = true"></AddCard>
+							<ObjectiveForm v-model="showObjectiveForm"></ObjectiveForm>
+						</v-expansion-panel-content>
+					</v-expansion-panel>
+				</v-expansion-panels>
 			</v-col>
 			<v-col cols="12" md="4" sm="2">
-				<v-card>
-					<v-card-title> {{ panels[1] }} </v-card-title>
-					<v-card-text>
-						<draggable
-							v-model="liveData.events"
-							group="events"
-							@start="drag = true"
-							@end="
-								drag = false;
-								persist();
-							"
-						>
-							<EventCard
-								class="draggable"
-								v-for="event in liveData.events"
-								:key="event.id"
-								v-bind="event"
-								:show-icon="true"
-							></EventCard>
-						</draggable>
-						<AddCard @add-card-clicked="showEventForm = true"></AddCard>
-						<EventForm v-model="showEventForm"></EventForm>
-					</v-card-text>
-				</v-card>
+				<v-expansion-panels v-model="isEventCollapsed">
+					<v-expansion-panel>
+						<v-expansion-panel-header class="text-h5 text--primary">
+							{{ panels[1] }}
+						</v-expansion-panel-header>
+						<v-expansion-panel-content>
+							<draggable
+								v-model="liveData.events"
+								group="events"
+								@start="drag = true"
+								@end="
+									drag = false;
+									persist();
+								"
+							>
+								<EventCard
+									class="draggable"
+									v-for="event in liveData.events"
+									:key="event.id"
+									v-bind="event"
+									:show-icon="true"
+								></EventCard>
+							</draggable>
+							<AddCard @add-card-clicked="showEventForm = true"></AddCard>
+							<EventForm v-model="showEventForm"></EventForm>
+						</v-expansion-panel-content>
+					</v-expansion-panel>
+				</v-expansion-panels>
 			</v-col>
 			<v-col cols="12" md="4" sm="2">
-				<v-card>
-					<v-card-title> {{ panels[2] }} </v-card-title>
-					<v-card-text>
-						<draggable
-							v-model="liveData.locations"
-							group="locations"
-							@start="drag = true"
-							@end="
-								drag = false;
-								persist();
-							"
-						>
-							<LocationCard
-								class="draggable"
-								v-for="location in liveData.locations"
-								:key="location.id"
-								v-bind="location"
-							></LocationCard>
-						</draggable>
-						<AddCard @add-card-clicked="showLocationForm = true"></AddCard>
-						<LocationForm v-model="showLocationForm"></LocationForm>
-					</v-card-text>
-				</v-card>
+				<v-expansion-panels v-model="isLocationCollapsed">
+					<v-expansion-panel>
+						<v-expansion-panel-header class="text-h5 text--primary">
+							{{ panels[2] }}
+						</v-expansion-panel-header>
+						<v-expansion-panel-content>
+							<draggable
+								v-model="liveData.locations"
+								group="locations"
+								@start="drag = true"
+								@end="
+									drag = false;
+									persist();
+								"
+							>
+								<LocationCard
+									class="draggable"
+									v-for="location in liveData.locations"
+									:key="location.id"
+									v-bind="location"
+								></LocationCard>
+							</draggable>
+							<AddCard @add-card-clicked="showLocationForm = true"></AddCard>
+							<LocationForm v-model="showLocationForm"></LocationForm>
+						</v-expansion-panel-content>
+					</v-expansion-panel>
+				</v-expansion-panels>
 			</v-col>
 			<v-col cols="12" md="4" sm="2">
-				<v-card>
-					<v-card-title> {{ panels[3] }} </v-card-title>
-					<v-card-text>
-						<draggable
-							v-model="liveData.characters"
-							group="characters"
-							@start="drag = true"
-							@end="
-								drag = false;
-								persist();
-							"
-						>
-							<CharacterCard
-								class="draggable"
-								v-for="character in liveData.characters"
-								:key="character.id"
-								v-bind="character"
-							></CharacterCard>
-						</draggable>
-						<AddCard @add-card-clicked="showCharacterForm = true"></AddCard>
-						<CharacterForm v-model="showCharacterForm"></CharacterForm>
-					</v-card-text>
-				</v-card>
+				<v-expansion-panels v-model="isCharacterCollapsed">
+					<v-expansion-panel>
+						<v-expansion-panel-header class="text-h5 text--primary">
+							{{ panels[3] }}
+						</v-expansion-panel-header>
+						<v-expansion-panel-content>
+							<draggable
+								v-model="liveData.characters"
+								group="characters"
+								@start="drag = true"
+								@end="
+									drag = false;
+									persist();
+								"
+							>
+								<CharacterCard
+									class="draggable"
+									v-for="character in liveData.characters"
+									:key="character.id"
+									v-bind="character"
+								></CharacterCard>
+							</draggable>
+							<AddCard @add-card-clicked="showCharacterForm = true"></AddCard>
+							<CharacterForm v-model="showCharacterForm"></CharacterForm>
+						</v-expansion-panel-content>
+					</v-expansion-panel>
+				</v-expansion-panels>
 			</v-col>
 			<v-col cols="12" md="4" sm="2">
-				<v-card>
-					<v-card-title> {{ panels[4] }} </v-card-title>
-					<v-card-text>
-						<draggable
-							v-model="liveData.notes"
-							group="notes"
-							@start="drag = true"
-							@end="
-								drag = false;
-								persist();
-							"
-						>
-							<NoteCard
-								class="draggable"
-								v-for="note in liveData.notes"
-								:key="note.id"
-								v-bind="note"
-							></NoteCard>
-						</draggable>
-						<AddCard @add-card-clicked="showNoteForm = true"></AddCard>
-						<NoteForm v-model="showNoteForm"></NoteForm>
-					</v-card-text>
-				</v-card>
+				<v-expansion-panels v-model="isNoteCollapsed">
+					<v-expansion-panel>
+						<v-expansion-panel-header class="text-h5 text--primary">
+							{{ panels[4] }}
+						</v-expansion-panel-header>
+						<v-expansion-panel-content>
+							<draggable
+								v-model="liveData.notes"
+								group="notes"
+								@start="drag = true"
+								@end="
+									drag = false;
+									persist();
+								"
+							>
+								<NoteCard
+									class="draggable"
+									v-for="note in liveData.notes"
+									:key="note.id"
+									v-bind="note"
+								></NoteCard>
+							</draggable>
+							<AddCard @add-card-clicked="showNoteForm = true"></AddCard>
+							<NoteForm v-model="showNoteForm"></NoteForm>
+						</v-expansion-panel-content>
+					</v-expansion-panel>
+				</v-expansion-panels>
 			</v-col>
 		</v-row>
 	</v-container>
@@ -163,7 +183,7 @@ export default {
 		LocationForm,
 		CharacterForm,
 		NoteForm,
-        draggable
+		draggable,
 	},
 	data() {
 		return {
@@ -174,13 +194,18 @@ export default {
 			showLocationForm: false,
 			showCharacterForm: false,
 			showNoteForm: false,
+			isObjectiveCollapsed: 0,
+			isEventCollapsed: 0,
+			isLocationCollapsed: 0,
+			isCharacterCollapsed: 0,
+			isNoteCollapsed: 0,
 		};
 	},
 	methods: {
 		persist() {
 			storage.persist();
-		}
-	}
+		},
+	},
 };
 </script>
 
