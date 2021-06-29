9a50f0b25fda863f35245469a8f956060ec8620f
const39 <const8539@gmail.com>
2021-06-29 16:10:27 +0200

    Change Timeline style

---
+++ src/components/TimelineEvent.vue
@@ -0,0 +1,76 @@
+<template>
+	<v-timeline-item v-if="nodeInfo.header" class="d-flex" hide-dot>
+		<div class="text--primary font-weight-medium">{{ nodeInfo.header }}</div>
+	</v-timeline-item>
+	<v-timeline-item v-else :icon="nodeInfo.icon" :color="nodeInfo.color" class="d-flex" fill-dot>
+		<div class="text--primary">{{ nodeInfo.text }}</div>
+	</v-timeline-item>
+</template>
+
+<script>
+import constants from "../js/constants";
+import icons from "../js/icons";
+import { Event } from "../js/model";
+
+export default {
+	props: {
+		item: {
+			type: [Event, String],
+			required: true,
+		},
+	},
+	computed: {
+        /**
+         * Return a custom object with the node's data, depending on the item prop type.
+         */
+		nodeInfo() {
+			let info = {};
+
+            /**
+             * If the item prop is an Event, return the relevant Event data.
+             * Otherwise, return the prop string in a 'header' field.
+             */
+			if (this.item instanceof Event) {
+				info.text = this.item.desc;
+				info.icon = icons[this.item.type];
+
+				switch (this.item.type) {
+					case constants.eventTypes.COMBAT:
+						info.color = "deep-orange darken-1";
+						break;
+					case constants.eventTypes.ENCOUNTER:
+						info.color = "purple darken-1";
+						break;
+					case constants.eventTypes.DISCOVERY:
+						info.color = "green lighten-1";
+						break;
+					case constants.eventTypes.TRAVEL:
+						info.color = "indigo";
+						break;
+					default:
+						info.color = "grey darken-2";
+						break;
+				}
+			} else info.header = this.item;
+
+			return info;
+		},
+	},
+};
+</script>
+<style>
+/* Set zoom on hover animation on the compiled Vuetify class of the dot */
+.v-timeline-item__dot:hover {
+	animation: zoom-on-hover 0.1s ease-in 1;
+	transform: scale(1.25);
+}
+
+@keyframes zoom-on-hover {
+	0% {
+		transform: scale(1);
+	}
+	100% {
+		transform: scale(1.25);
+	}
+}
+</style>
--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -2,21 +2,8 @@
 	<v-container>
 		<div class="my-3">
 			<div class="text-xl-h4">Frise des événements</div>
-			<v-timeline v-if="events.length > 0">
-				<v-timeline-item icon="mdi-star" color="yellow accent-4" fill-dot>
-					<BaseCard>
-						<v-card-text class="pa-3 text--primary">Début de la campagne</v-card-text>
-					</BaseCard>
-				</v-timeline-item>
-				<v-timeline-item
-					v-for="event in events"
-					:key="event.id"
-					:icon="icons[event.type]"
-					:color="computeColor(event.type)"
-					fill-dot
-				>
-					<CardEvent :item-data="event" :hide-icon="true" />
-				</v-timeline-item>
+			<v-timeline dense class="mx-16 my-6" v-if="nodes.length > 0">
+				<TimelineEvent v-for="node in nodes" :key="node.id || node" :item="node" />
 			</v-timeline>
 			<p class="text-center" v-else>
 				Aucun événement enregistré. Vous pouvez en ajouter sur le
@@ -26,61 +13,23 @@
 	</v-container>
 </template>
 <script>
-import constants from "../js/constants.js";
-import icons from "../js/icons.js";
 
-import CardEvent from "../components/cards/CardEvent.vue";
-import BaseCard from "../components/cards/BaseCard.vue";
+import TimelineEvent from "../components/TimelineEvent.vue";
 
 export default {
 	components: {
-		CardEvent,
-		BaseCard,
-	},
-	data() {
-		return {
-			icons: icons,
-		};
-	},
-	methods: {
-		computeColor(eventType) {
-			switch (eventType) {
-				case constants.eventTypes.COMBAT:
-					return "deep-orange darken-1";
-				case constants.eventTypes.ENCOUNTER:
-					return "purple darken-1";
-				case constants.eventTypes.DISCOVERY:
-					return "green lighten-1";
-				case constants.eventTypes.TRAVEL:
-					return "indigo";
-				default:
-					return "grey darken-2";
-			}
-		},
+		TimelineEvent,
 	},
 	computed: {
-		events() {
+		nodes() {
 			// Get events from store and create a copy of the array to reverse it
 			let copy = this.$store.state.cards.events.slice();
-			return copy.reverse();
+			copy.reverse();
+
+			copy.splice(0, 0, "Début de la campagne")
+			return copy;
 		},
 	},
 };
 </script>
 
-<style>
-/* Set zoom on hover animation on the compiled Vuetify class of the dot */
-.v-timeline-item__dot:hover {
-	animation: zoom-on-hover 0.1s ease-in 1;
-	transform: scale(1.25);
-}
-
-@keyframes zoom-on-hover {
-	0% {
-		transform: scale(1);
-	}
-	100% {
-		transform: scale(1.25);
-	}
-}
-</style>
