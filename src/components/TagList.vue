1d583c2d10575f94199be196cc67d439f5ac72ab
const39 <const8539@gmail.com>
2022-02-20 16:43:17 +0100

    Reworked card Tag list design

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,22 +1,20 @@
 <template>
-	<div>
-		<v-chip v-for="tag in tags" :key="tag.id" class="ma-1" small @click.stop="goToCard(tag.type, tag.id)">
-			<v-icon left>{{ tag.icon }}</v-icon>
-			<MarkdownView class="text-truncate" :text="tag.text | truncate" :inline="true" />
-		</v-chip>
+	<div class="pa-3">
+		<v-row v-for="(list, type) in tags" :key="type">
+			<v-icon small>{{icons[type]}}</v-icon>
+			<v-chip v-for="tag in list" :key="tag.id" class="ma-1" small outlined @click.stop="goToCard(tag.type, tag.id)">
+				{{tag.text | truncate}}
+			</v-chip>
+		</v-row>
 	</div>
 </template>
 
 <script>
-import { Tag } from "../js/model.js";
+import { Tag, Objective, Event, Location, Character, Note } from "../js/model.js";
 import { eventHub, TagEvent } from "../js/eventHub.js";
-
-import MarkdownView from "./MarkdownView.vue";
+import constants from "../js/constants.js";
 
 export default {
-	components: {
-		MarkdownView,
-	},
 	props: {
 		items: {
 			// Must be an array of IDs
@@ -25,6 +23,11 @@ export default {
 				return [];
 			},
 		},
+	},
+	data() {
+		return {
+			icons: constants.icons
+		}
 	},
 	methods: {
 		/**
@@ -40,13 +43,30 @@ export default {
 		 * Create a Tag for each object whose ID is given
 		 */
 		tags() {
-			let tagsList = [];
-			for (const id of this.items) {
+			let tagsList = {};
 
+			for (const id of this.items) {
 				const elem = this.$store.getters.get(id);
-				
+
 				// If the object is found, create a tag object from the element's data
-				if (elem) tagsList.push(new Tag(elem));
+				if (elem) {
+					let key = undefined;
+					if (elem instanceof Objective) key = constants.objectTypes.OBJECTIVE;
+					else if (elem instanceof Event) key = constants.objectTypes.EVENT;
+					else if (elem instanceof Location) key = constants.objectTypes.LOCATION;
+					else if (elem instanceof Character) key = constants.objectTypes.CHARACTER;
+					else if (elem instanceof Note) key = constants.objectTypes.NOTE;
+					else {
+						console.error(elem, "is not an instance of an accepted object.");
+						return;
+					}
+
+					// Create a key of the object's type if there is none yet
+					if(!(key in tagsList)) tagsList[key] = []
+
+					// Add the tag in the list of its type
+					tagsList[key].push(new Tag(elem));
+				}
 			}
 			return tagsList;
 		},
