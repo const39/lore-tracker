3cc22c002ee49299ba1d9f75ac3f33e13aa99359
const39 <const8539@gmail.com>
2022-06-03 18:02:41 +0200

    QuickNote component restyle

--- src/components/QuickNote.vue
+++ src/components/QuickNote.vue
@@ -1,16 +1,37 @@
 <template>
 	<div>
-		<v-expand-transition>
-			<v-card v-if="opened">
+		<v-fab-transition origin="bottom right">
+			<v-card v-if="open">
 				<v-card-text>
-					<v-textarea outlined auto-grow autofocus :hint="$t('fields.mdSupport')" v-model="text"></v-textarea>
+					<div class="d-flex justify-space-between">
+						<v-btn
+							class="resizing-handle"
+							icon
+							@mousedown="onHold"
+							@mousemove="resize"
+							@mouseup="resizing = false"
+						>
+							<v-icon>mdi-cursor-move</v-icon>
+						</v-btn>
+						<p class="text-h6 text--primary">{{ $t("actions.quickNote") }}</p>
+						<v-btn icon @click="open = false">
+							<v-icon>mdi-chevron-down</v-icon>
+						</v-btn>
+					</div>
+					<v-textarea
+						id="resizable"
+						outlined
+						auto-grow
+						autofocus
+						:hint="$t('fields.mdSupport')"
+						v-model="text"
+					></v-textarea>
 				</v-card-text>
 			</v-card>
-		</v-expand-transition>
-		<v-btn class="text-left" :rounded="!opened" block color="primary" @click="opened = !opened">
-			<v-icon :class="{ 'rotation': opened }">mdi-chevron-up</v-icon>
-			{{ $t('actions.quickNote') }}
-		</v-btn>
+			<v-btn v-else fab color="primary" @click="open = true">
+				<v-icon>mdi-text-box-edit</v-icon>
+			</v-btn>
+		</v-fab-transition>
 	</div>
 </template>
 
@@ -18,10 +39,55 @@
 export default {
 	data() {
 		return {
-			opened: false,
+			open: false,
+			resizing: false,
+			originalW: 0,
+			originalH: 0,
+			originalX: 0,
+			originalY: 0,
+			originalMouseX: 0,
+			originalMouseY: 0,
 		};
 	},
-    computed: {
+	methods: {
+		onHold(e) {
+			const element = document.getElementById("resizable");
+			e.preventDefault();
+			this.originalW = Number.parseFloat(
+				getComputedStyle(element, null)
+					.getPropertyValue("width")
+					.replace("px", "")
+			);
+			this.originalH = Number.parseFloat(
+				getComputedStyle(element, null)
+					.getPropertyValue("height")
+					.replace("px", "")
+			);
+			this.originalX = element.getBoundingClientRect().left;
+			this.originalY = element.getBoundingClientRect().top;
+			this.originalMouseX = e.pageX;
+			this.originalMouseY = e.pageY;
+
+			this.resizing = true;
+		},
+		resize(e) {
+			const element = document.getElementById("resizable");
+			const MIN_SIZE = 20;
+			if (this.resizing) {
+				const width = this.originalW - (e.pageX - this.originalMouseX);
+				const height = this.originalH - (e.pageY - this.originalMouseY);
+				if (width > MIN_SIZE) {
+					element.style.width = width + "px";
+					element.style.left = this.originalX + (e.pageX - this.originalMouseX) + "px";
+				}
+				if (height > MIN_SIZE) {
+					element.style.height = height + "px";
+					element.style.top = this.originalY + (e.pageY - this.originalMouseY) + "px";
+				}
+			}
+		},
+	},
+	computed: {
 		text: {
 			get() {
 				return this.$store.state.quickNote;
@@ -35,17 +101,7 @@ export default {
 </script>
 
 <style scoped>
-.rotation {
-	animation: rotate 0.1s linear 1;
-	transform: rotate(180deg);
-}
-
-@keyframes rotate {
-	from {
-		transform: rotate(0deg);
-	}
-	to {
-		transform: rotate(180deg);
-	}
+.resizing-handle {
+	cursor: nw-resize;
 }
 </style>
