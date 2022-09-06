b6782658bd45801b30909666a6202e5b42cb01d5
const39 <const8539@gmail.com>
2022-09-06 13:22:26 +0200

    Remove unused electron dependencies

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,7 +31,7 @@ const routes = [
 
 const router = new VueRouter({
 	routes,
-	mode: process.env.IS_ELECTRON ? "hash" : "history",
+	mode: "history",
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,3 @@
 module.exports = {
 	transpileDependencies: ["vuetify"],
-	pluginOptions: {
-		electronBuilder: {
-			builderOptions: {
-				appId: "com.electron.lore-tracker",
-				productName: "Lore Tracker",
-				win: {
-					target: ["nsis", "portable"],
-				},
-				nsis: {
-					license: "LICENSE",
-					artifactName: "${productName}_${version}_Installer.${ext}",
-					oneClick: false,
-					perMachine: false,
-					allowToChangeInstallationDirectory: true
-				},
-				portable: {
-					artifactName: "${productName}_${version}_Portable.${ext}",
-				},
-			},
-		},
-	},
 };
