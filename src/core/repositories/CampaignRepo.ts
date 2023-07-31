999b8df2b673c7f63f39dbad7bd729baabf0238f
const39 <const8539@gmail.com>
2023-07-31 14:01:58 +0200

    fix: deleting campaign did not delete its related lore entries

--- src/core/repositories/CampaignRepo.ts
+++ src/core/repositories/CampaignRepo.ts
@@ -2,9 +2,24 @@ import { Campaign, Category, Folder } from "../models";
 import { UUID } from "../utils/types";
 import BaseRepo, { QueryOptions } from "./BaseRepo";
 import FolderRepo from "./FolderRepo";
+import LoreEntryRepo from "./LoreEntryRepo";
 
 export default class CampaignRepo extends BaseRepo<Campaign> {
 	use = Campaign;
+
+	/**
+	 * Delete an existing campaign.
+	 * This will also recursively delete all its folders and lore entries.
+	 *
+	 * @param id the ID of the campaign to delete
+	 */
+	override delete(id: UUID): void {
+		// Delete the campaign
+		super.delete(id);
+		// Delete its related folders and lore entries
+		this.repo(LoreEntryRepo).where("campaignId", id).delete();
+		this.repo(FolderRepo).where("campaignId", id).delete();
+	}
 
 	using(campaign: Campaign, options?: QueryOptions) {
 		return this.repo(FolderRepo).createQuery(options).where("campaignId", campaign.id);
